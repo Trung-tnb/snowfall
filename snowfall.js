2947c0ca4ac8e62118a83a72fa35ab74b8c56fb4
Trung-tnb <113741349+Trung-tnb@users.noreply.github.com>
2023-12-07 14:15:38 +0700

    snowfall.js

--- snowfall.js
+++ snowfall.js
@@ -1,67 +1,33 @@
-document.write('<style type="text/css">body{padding-bottom:20px}</style><img style="position:fixed;z-index:9999;top:0;left:0" src="https://sharethuthuat.com/wp-content/uploads/2022/12/pine-tree-left-new.png"/><img style="position:fixed;z-index:9999;top:0;right:0" src="https://sharethuthuat.com/wp-content/uploads/2022/12/pine-tree-right.png"><div style="position:fixed;z-index:9999;bottom:-50px;left:0;width:100%;height:104px;background:url(https://sharethuthuat.com/wp-content/uploads/2021/12/ft.png) repeat-x bottom left;"></div><img style="position:fixed;z-index:9999;bottom:20px;left:20px" src="https://sharethuthuat.com/wp-content/uploads/2021/12/bottomleft.png"/><img style="position:fixed;z-index:9999;bottom:20px;right:20px" src="https://sharethuthuat.com/wp-content/uploads/2022/12/pine-tree-bottom.png"/><div style="position:fixed;z-index:9999;bottom:3px;right:3px; font-size:1px;color:#8D8D8D;">by <a href="https://sharethuthuat.com">Share Thá»§ Thuáº­t</a></div>');
-var no = 100;
-var hidesnowtime = 0;
-var snowdistance = 'pageheight';
-var ie4up = (document.all) ? 1 : 0;
-var ns6up = (document.getElementById && !document.all) ? 1 : 0;
+document.addEventListener("DOMContentLoaded", function () {
+  var snowflakesContainer = document.createElement("div");
+  snowflakesContainer.classList.add("snowflakes");
+  snowflakesContainer.setAttribute("aria-hidden", "true");
+  document.body.appendChild(snowflakesContainer);
 
-function iecompattest() {
-	return (document.compatMode && document.compatMode != 'BackCompat') ? document.documentElement : document.body
-}
-var dx, xp, yp;
-var am, stx, sty;
-var i, doc_width = 800,
-	doc_height = 600;
-if (ns6up) {
-	doc_width = self.innerWidth;
-	doc_height = self.innerHeight
-} else if (ie4up) {
-	doc_width = iecompattest().clientWidth;
-	doc_height = iecompattest().clientHeight
-}
-dx = new Array();
-xp = new Array();
-yp = new Array();
-am = new Array();
-stx = new Array();
-sty = new Array();
-for (i = 0; i < no; ++i) {
-	dx[i] = 0;
-	xp[i] = Math.random() * (doc_width - 50);
-	yp[i] = Math.random() * doc_height;
-	am[i] = Math.random() * 20;
-	stx[i] = 0.02 + Math.random() / 10;
-	sty[i] = 0.7 + Math.random();
-	if (ie4up || ns6up) {
-		document.write('<div id="dot' + i + '" style="POSITION:absolute;Z-INDEX:' + i + ';VISIBILITY:visible;TOP:15px;LEFT:15px;"><span style="font-size:18px;color:#fff">*</span></div>')
-	}
-}
+  for (var i = 0; i < 12; i++) {
+    var snowflake = document.createElement("div");
+    snowflake.classList.add("snowflake");
+    snowflakesContainer.appendChild(snowflake);
 
-function snowIE_NS6() {
-	doc_width = ns6up ? window.innerWidth - 10 : iecompattest().clientWidth - 10;
-	doc_height = (window.innerHeight && snowdistance == 'windowheight') ? window.innerHeight : (ie4up && snowdistance == 'windowheight') ? iecompattest().clientHeight : (ie4up && !window.opera && snowdistance == 'pageheight') ? iecompattest().scrollHeight : iecompattest().offsetHeight;
-	for (i = 0; i < no; ++i) {
-		yp[i] += sty[i];
-		if (yp[i] > doc_height - 50) {
-			xp[i] = Math.random() * (doc_width - am[i] - 30);
-			yp[i] = 0;
-			stx[i] = 0.02 + Math.random() / 10;
-			sty[i] = 0.7 + Math.random()
-		}
-		dx[i] += stx[i];
-		document.getElementById('dot' + i).style.top = yp[i] + 'px';
-		document.getElementById('dot' + i).style.left = xp[i] + am[i] * Math.sin(dx[i]) + 'px'
-	}
-	snowtimer = setTimeout('snowIE_NS6()', 10)
-}
+    var innerSnowflake = document.createElement("div");
+    innerSnowflake.classList.add("inner");
+    innerSnowflake.innerText = "❅";
+    snowflake.appendChild(innerSnowflake);
+  }
 
-function hidesnow() {
-	if (window.snowtimer) {
-		clearTimeout(snowtimer)
-	}
-	for (i = 0; i < no; i++) document.getElementById('dot' + i).style.visibility = 'hidden'
-}
-if (ie4up || ns6up) {
-	snowIE_NS6();
-	if (hidesnowtime > 0) setTimeout('hidesnow()', hidesnowtime * 1000)
-}
+  var promotionVisible = false;
+
+  function handleScroll() {
+    if (window.scrollY > 50 && !promotionVisible) {
+      snowflakesContainer.classList.add("visible");
+      promotionVisible = true;
+    } else if (window.scrollY <= 50 && promotionVisible) {
+      snowflakesContainer.classList.remove("visible");
+      promotionVisible = false;
+    }
+  }
+
+  window.addEventListener("scroll", handleScroll);
+  handleScroll(); // Xử lý ngay khi trang được tải để kiểm tra trạng thái ban đầu
+});
+
